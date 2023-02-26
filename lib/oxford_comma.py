ce64a9fbaa1fa2b7b680c2ebab5382fb7d23b747
JaVonn Williams <javonn_w@yahoo.com>
2023-02-25 16:12:42 -0800

    happy day

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        # Oxford comma: insert comma before the final item
+        last_item = items.pop()
+        item_str = ", ".join(items)
+        return f"{item_str}, and {last_item}"
